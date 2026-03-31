0bcb5a61ef1200b041d3886473b60603bf9d5ca3
DaniilEvseev <Danchic2033@yandex.ru>
2026-03-31 07:33:03 +0500

    рефакторинг History Products

--- OzonLookWinForm/HistoryProductsRepository.cs
+++ OzonLookWinForm/HistoryProductsRepository.cs
@@ -1,10 +1,4 @@
 ﻿using Library;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tensorflow;
 
 namespace OzonLookWinForm
 {
