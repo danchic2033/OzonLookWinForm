adc6334f7fb9cb2ed37772a98f8fa8a3852182d8
DaniilEvseev <Danchic2033@yandex.ru>
2026-03-31 07:25:02 +0500

    Рефакторинг FavoriteProducts и FavoriteProductsRepository

--- OzonLookWinForm/FavoriteProductsRepository.cs
+++ OzonLookWinForm/FavoriteProductsRepository.cs
@@ -1,9 +1,4 @@
 ﻿using Library;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OzonLookWinForm
 {
@@ -19,7 +14,6 @@ namespace OzonLookWinForm
         }
         public static List<FavoriteProducts> GetProduct()
         {
-            
             return _favoriteProducts;
         }
     }
